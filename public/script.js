94964d2e193d0f3d908c78b06897f302adf2a386
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-04-22 10:26:10 +0400

    Ajuste la position mobile du compteur de voix

--- public/script.js
+++ public/script.js
@@ -1632,7 +1632,7 @@ function ensureDebateIframeModal() {
     }
     @media (max-width: 768px) {
       #debate-iframe-modal-close {
-        bottom: 12px;
+        bottom: calc(5vh + 66px);
       }
     }
     @media (min-width: 769px) {
