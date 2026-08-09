37998c0d14992de2eec9897e2773d47f49cc0afc
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-08-10 00:02:39 +0400

    Améliore la lisibilité du message mémoire

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "20260809-ui-fixes-cache-bust-v137";
+const SW_VERSION = "20260810-ui-fixes-cache-bust-v138";
 const STATIC_CACHE = `agon-static-${SW_VERSION}`;
 const NAVIGATION_FETCH_TIMEOUT_MS = 8000;
 
