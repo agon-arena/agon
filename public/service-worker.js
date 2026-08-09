a9641b1dc3cf4976255009b11a58506ff24c6959
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-08-09 23:51:11 +0400

    Aligne l’espace de la mémoire vide

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "20260809-ui-fixes-cache-bust-v132";
+const SW_VERSION = "20260809-ui-fixes-cache-bust-v133";
 const STATIC_CACHE = `agon-static-${SW_VERSION}`;
 const NAVIGATION_FETCH_TIMEOUT_MS = 8000;
 
