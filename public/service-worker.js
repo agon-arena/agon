855e5ea8c9e60ef60e3cfba44ea20d8d4f4a9743
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-08-10 00:20:42 +0400

    Aligne les liens standalone sur le mobile

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "20260810-ui-fixes-cache-bust-v144";
+const SW_VERSION = "20260810-ui-fixes-cache-bust-v145";
 const STATIC_CACHE = `agon-static-${SW_VERSION}`;
 const NAVIGATION_FETCH_TIMEOUT_MS = 8000;
 
