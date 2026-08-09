b7d852ab9e8666cd1e5d0db2607c2378b2cd74eb
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-08-09 22:50:00 +0400

    Renomme Univers en Apprentissage

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "20260809-ui-fixes-cache-bust-v119";
+const SW_VERSION = "20260809-ui-fixes-cache-bust-v120";
 const STATIC_CACHE = `agon-static-${SW_VERSION}`;
 const NAVIGATION_FETCH_TIMEOUT_MS = 8000;
 
