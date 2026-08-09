7a6561177aa49aec45b55c890b62ce0b934bffe4
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-08-10 00:17:52 +0400

    Réaligne les liens secondaires en standalone

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "20260810-ui-fixes-cache-bust-v143";
+const SW_VERSION = "20260810-ui-fixes-cache-bust-v144";
 const STATIC_CACHE = `agon-static-${SW_VERSION}`;
 const NAVIGATION_FETCH_TIMEOUT_MS = 8000;
 
