eb9bb05a94ebf42eefa79a19902488368226d5d6
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-08-09 21:46:22 +0400

    message de commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "20260809-memoire-bg-preload1-cache-bust-v114";
+const SW_VERSION = "20260809-ui-fixes-cache-bust-v115";
 const STATIC_CACHE = `agon-static-${SW_VERSION}`;
 const NAVIGATION_FETCH_TIMEOUT_MS = 8000;
 
