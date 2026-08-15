e0a69229995a9180846a6e56ca22a4dbdc78aeaf
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-08-15 12:54:50 +0400

    Standardize Mnoria header sizing

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "20260815-mnoria-startup-logo-v175";
+const SW_VERSION = "20260815-uniform-mnoria-header-v176";
 const STATIC_CACHE = `agon-static-${SW_VERSION}`;
 const NAVIGATION_FETCH_TIMEOUT_MS = 8000;
 
