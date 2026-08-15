e7cae9335765c3ed069ee36e63a0b40d4c63a137
Kevin Bruyat <kevinbruyat@MacBook-Air-de-Kevin.local>
2026-08-15 12:43:18 +0400

    Show Mnoria logo in startup animation

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "20260815-mnoria-header-visible-v174";
+const SW_VERSION = "20260815-mnoria-startup-logo-v175";
 const STATIC_CACHE = `agon-static-${SW_VERSION}`;
 const NAVIGATION_FETCH_TIMEOUT_MS = 8000;
 
